d82fb5162aa1da91550c419b34a031a63014ef92
Anatoliy Serputov <aserputov@myseneca.ca>
2023-01-01 15:36:38 -0500

    Additional feature to run with websockets

--- consume.js
+++ consume.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const kafka = require("kafka-node");
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 8080 });
 // const fs = require("fs");
 // const ejs = require("ejs");
 
 const app = express();
-const port = 3001;
+// const port = 3001;
 
 const client = new kafka.KafkaClient({ kafkaHost: "localhost:9092" });
 const consumer = new kafka.Consumer(client, [{ topic: "events" }], {
@@ -14,15 +16,20 @@ const consumer = new kafka.Consumer(client, [{ topic: "events" }], {
 app.set("view engine", "ejs");
 
 const messages = [];
-consumer.on("message", (message) => {
-  messages.push(message.value);
-  console.log(messages);
+wss.on("connection", (ws) => {
+  consumer.on("message", (message) => {
+    ws.send(message.value);
+  });
 });
+// consumer.on("message", (message) => {
+//   messages.push(message.value);
+//   console.log(messages);
+// });
 
 app.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
